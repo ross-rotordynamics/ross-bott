25825306ee7d99065950a3f3f7e46113a026ce65
raphaeltimbo <raphaelts@gmail.com>
2019-12-30 20:56:58 -0300

    Add while loop within mark function

--- ross-bott/__main__.py
+++ ross-bott/__main__.py
@@ -19,11 +19,9 @@ sentry_logging = LoggingIntegration(
     event_level=logging.INFO   # Send errors as events
 )
 sentry_sdk.init(
-    dsn="https://de5148635d84489d9451f404e2d8fb17@sentry.io/1868960",
+    dsn=os.environ.get("SENTRY_DSN"),
     integrations=[sentry_logging]
 )
-
-sentry_sdk.init(dsn=os.environ.get("SENTRY_DSN"))
 
 
 routes = web.RouteTableDef()
@@ -72,34 +70,35 @@ ross_repo = g.get_repo("ross-rotordynamics/ross")
 
 def mark_stale_issues():
     # days limit for staled issues
-    print(f'Running "mark_stale_issues" at {datetime.now()}')
-    logging.debug(f'Running "mark_stale_issues" at {datetime.now()}')
+    while True:
+        print(f'Running "mark_stale_issues" at {datetime.now()}')
+        logging.debug(f'Running "mark_stale_issues" at {datetime.now()}')
 
-    LIMIT = 45
+        LIMIT = 45
 
-    issues = ross_repo.get_issues(state="open")
-    not_updated_issues = []
-    for issue in issues:
-        last_update = (datetime.today() - issue.updated_at).days
-        if last_update > LIMIT:
-            not_updated_issues.append(issue)
+        issues = ross_repo.get_issues(state="open")
+        not_updated_issues = []
+        for issue in issues:
+            last_update = (datetime.today() - issue.updated_at).days
+            if last_update > LIMIT:
+                not_updated_issues.append(issue)
 
-    # fmt: off
-    stale_message = (
-        f'Hi there!\n'
-        f'I have marked this issue as stale because it has not had activity for {LIMIT} days.\n'
-        f'Consider the following options:\n'
-        f'- If the issue refers to a large task, break it in smaller issues that can be solved in\n'
-        f'less than {LIMIT} days;\n'
-        f'- Label the issue as `wontfix` or `wontfix for now` and close it.'
-    )
-    # fmt: on
+        # fmt: off
+        stale_message = (
+            f'Hi there!\n'
+            f'I have marked this issue as stale because it has not had activity for {LIMIT} days.\n'
+            f'Consider the following options:\n'
+            f'- If the issue refers to a large task, break it in smaller issues that can be solved in\n'
+            f'less than {LIMIT} days;\n'
+            f'- Label the issue as `wontfix` or `wontfix for now` and close it.'
+        )
+        # fmt: on
 
-    # for issue in not_updated_issues:
-    #     issue.create_comment(stale_message)
-    #     issue.add_to_labels("stale")
-    print(not_updated_issues)
-    time.sleep(10)
+        # for issue in not_updated_issues:
+        #     issue.create_comment(stale_message)
+        #     issue.add_to_labels("stale")
+        print(not_updated_issues)
+        time.sleep(10)
 
 
 if __name__ == "__main__":

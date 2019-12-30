c04d3d2fc2ce81796f27786bb4eccad0f5a2975b
raphaeltimbo <raphaelts@gmail.com>
2019-12-30 15:05:28 -0300

    Trying to use gidgethub

--- ross-bott/__main__.py
+++ ross-bott/__main__.py
@@ -3,13 +3,50 @@
 import os
 import time
 import schedule
+import aiohttp
 from github import Github as gh
 from datetime import datetime
-import sentry_sdk
-sentry_sdk.init(os.environ['SENTRY_DSN'])
+from aiohttp import web
 
-divi0 = 1/0
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
 
+routes = web.RouteTableDef()
+router = routing.Router()
+
+
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """
+    Whenever an issue is opened, greet the author and say thanks.
+    """
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
+
+@routes.post("/")
+async def main(request):
+    # read the GitHub webhook payload
+    body = await request.read()
+
+    # our authentication token and secret
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
+
+    # a representation of GitHub webhook event
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "ross-bott",
+                                  oauth_token=oauth_token)
+
+        # call the appropriate callback for the event
+        await router.dispatch(event, gh)
+
+    # return a "Success"
+    return web.Response(status=200)
 
 token = os.environ.get("GH_AUTH")
 g = gh(token)
@@ -46,6 +83,13 @@ def mark_stale_issues():
 
 if __name__ == "__main__":
     print("Started app.")
+    app = web.Application()
+    app.add_routes(routes)
+    port = os.environ.get("PORT")
+    if port:
+        port = int(port)
+
+    web.run_app(app, port=port)
     schedule.every(1).minutes.do(mark_stale_issues)
 
     while True:

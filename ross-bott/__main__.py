b0303ac8e5ce2e2c72de79fae8cab6db57fe450d
raphaeltimbo <raphaelts@gmail.com>
2020-01-02 07:50:06 -0300

    Remove greeting

--- ross-bott/__main__.py
+++ ross-bott/__main__.py
@@ -37,12 +37,7 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     """
     Whenever an issue is opened, greet the author and say thanks.
     """
-    print("Running issue_opened_event.")
-    url = event.data["issue"]["comments_url"]
-    author = event.data["issue"]["user"]["login"]
-
-    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
-    await gh.post(url, data={"body": message})
+    pass
 
 
 @routes.post("/")
